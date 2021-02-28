b33170a9f22f92eb1635cf1cd2d7546c5e50c496
Victor <vlourenco.m@gmail.com>
2021-02-28 19:14:52 -0300

    Set favorite context & tried to order repositories

--- src/context/RepositoriesContext.jsx
+++ src/context/RepositoriesContext.jsx
@@ -3,20 +3,30 @@ import api from '../services/api';
 
 export const RepositoriesContext = createContext();
 
-export function RepositoriesProvider({ children, ...rest }) {
+export function RepositoriesProvider({ children }) {
   const [repository, setRepository] = useState('');
   const [reposList, setReposList] = useState([]);
   const [isPopoverActive, setIsPopoverActive] = useState(false);
   const [isModalActive, setIsModalActive] = useState(false);
+  const [hasError, setHasError]  = useState(false);
+  const [favorite, setFavorite] = useState([]);
+  const [isFavoritedPageActive, setIsFavoritedPageActive] = useState(false);
+  const [favoriteChecked, setFavoriteChecked] = useState(false);
+  const [infoToDeleteRepo, setInfoToDeleteRepo] = useState(0);
+  const [findRepo, setFindRepo] = useState('');
+  const [findedRepo, setFindedRepo] = useState({});
 
   const showModal = useCallback(() => {
     setIsModalActive(active => !active);
   }, []);
 
-  const [hasError, setHasError]  = useState(false);
-
   const showPopover = useCallback(() => {
-    setIsPopoverActive(active => !active)
+    setIsPopoverActive(active => !active);
+  }, []);
+
+  const showFavorites = useCallback(() => {
+    setFavoriteChecked(active => !active);
+    setIsFavoritedPageActive(active => !active);
   }, []);
 
   const handleAddRepository = useCallback(async (e) => {
@@ -26,6 +36,7 @@ export function RepositoriesProvider({ children, ...rest }) {
 
       setIsPopoverActive(false);
       setReposList([...reposList, response.data])
+      setRepository('');
     } catch (error) {
       setHasError(true);
     }
@@ -33,12 +44,64 @@ export function RepositoriesProvider({ children, ...rest }) {
 
   const handleDeleteReposiory = useCallback((id) => {
     reposList.map((repo, index) => {
-      console.log(repo.id);
       if (repo.id === id) {
         return reposList.splice(index, 1);
       }
     })
-  }, [reposList]);
+
+    favorite.map((repo, index) => {
+      if (repo.id === id) {
+        return favorite.splice(index, 1);
+      }
+    })
+  }, [reposList, favorite]);
+
+  const favoriteRepo = useCallback((id, isFavorited) => {
+      reposList.map(repo => {
+        if (repo.id === id && !isFavorited) {
+          console.log('colocou', !isFavorited)
+          setFavorite([...favorite, repo]);
+        }
+      })
+
+      favorite.map((repo, index) => {
+        if (repo.id === id && isFavorited ) {
+          console.log('retirou', isFavorited)
+          return favorite.splice(index, 1);
+        }
+      })
+  }, [reposList, favorite]);
+
+  const handleOrder = useCallback((order) => {
+    switch (order) {
+      case "star":
+        console.log('entrou star');
+        return (reposList.sort((a, b) => {
+          if (a.stargazers_count > b.stargazers_count) {
+            return 1;
+          }
+          if (a.stargazers_count < b.stargazers_count) {
+            return -1;
+          }
+          return 0;
+        }));
+      case "forks":
+        console.log('entrou forks');
+        return reposList.sort((a, b) => a.forks_count - b.forks_count).map(p => p);
+      case "open_issues":
+        return reposList.sort((a, b) => a.open_issues_count - b.open_issues_count).map(p => p);
+      default:
+        return reposList.map(p => p);
+    }
+  }, [reposList])
+
+  const handleFindRepo = useCallback(() => {
+    reposList.map(repo => {
+      if (findRepo === repo.full_name) {
+        return setFindedRepo(findedRepo);
+      }
+    })
+  }, []);
 
   return (
     <RepositoriesContext.Provider
@@ -53,6 +116,18 @@ export function RepositoriesProvider({ children, ...rest }) {
         showModal,
         isModalActive,
         handleDeleteReposiory,
+        favoriteRepo,
+        showFavorites,
+        isFavoritedPageActive,
+        favorite,
+        setInfoToDeleteRepo,
+        infoToDeleteRepo,
+        favoriteChecked,
+        handleOrder,
+        setFindRepo,
+        handleFindRepo,
+        findedRepo,
+        findRepo,
       }}
     >
       {children}
