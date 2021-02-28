ba1fb4c47d32d95b689855e2002fa749ae6ecc18
Victor <vlourenco.m@gmail.com>
2021-02-28 11:40:06 -0300

    Create delete modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,56 @@
+
+import React, { useContext } from 'react';
+import ClayButton from '@clayui/button';
+import { FiX } from 'react-icons/fi';
+import { GoAlert } from 'react-icons/go';
+import { RepositoriesContext } from '../context/RepositoriesContext';
+
+import styles from '../styles/components/Modal.module.css';
+
+const Modal = ({ repoId }) => {
+  const { showModal, handleDeleteReposiory } = useContext(RepositoriesContext);
+
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <header>
+          <div>
+            <GoAlert size={14} color="#b95000" />
+            <strong>Delete Repository</strong>
+          </div>
+          <button onClick={showModal} className={styles.button}>
+            <FiX size={24} color="#b95000" />
+          </button >
+        </header>
+        <main>
+          <p>Are you sure to delete the
+            <span>
+              <strong> liferay/senna.js </strong>
+            </span>
+           repository?</p>
+        </main>
+        <div className={styles.buttonContainer}>
+          <ClayButton
+            style={{ marginRight: 16 }}
+            displayType="secondary"
+            onClick={showModal}
+          >
+            Cancel
+          </ClayButton>
+          <ClayButton
+            displayType="unstyled"
+            className={styles.deleteButton}
+            onClick={() => {
+              handleDeleteReposiory(repoId);
+              showModal();
+            }}
+          >
+            Delete
+          </ClayButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
