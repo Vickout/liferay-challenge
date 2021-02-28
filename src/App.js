06f83790bc57a49b8b37f077f52de965756fcbe0
Victor <vlourenco.m@gmail.com>
2021-02-28 11:41:44 -0300

    Finish homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RepositoriesContext } from './context/RepositoriesContext';
+import EmptyPage from './pages/EmptyPage';
+import GridPage from './pages/GridPage';
+
 import Toolbar from './components/Toobar';
+import Popover from './components/Popover';
+
+
+import './styles/global.css';
 
 function App() {
+  const { isPopoverActive, reposList } = useContext(RepositoriesContext);
+
   return (
-    <Toolbar />
+    <>
+      <Toolbar />
+      { reposList.length !== 0 ? (
+        <>
+        <GridPage />
+        </>
+      ) : (
+        <EmptyPage />
+      )}
+      { isPopoverActive && <Popover />}
+    </>
   );
 }
 
